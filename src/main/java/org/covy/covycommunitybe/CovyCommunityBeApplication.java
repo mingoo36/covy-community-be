b6a42918d3258f8dbbe963761d9a8d48c73791e5
이민구 <goorm@goormui-MacBookPro.local>
2025-02-26 23:48:59 +0900

    Init: 스프링 프로젝트 생성

---
+++ src/main/java/org/covy/covycommunitybe/CovyCommunityBeApplication.java
@@ -0,0 +1,13 @@
+package org.covy.covycommunitybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovyCommunityBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovyCommunityBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/covy/covycommunitybe/CovyCommunityBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.covy.covycommunitybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovyCommunityBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

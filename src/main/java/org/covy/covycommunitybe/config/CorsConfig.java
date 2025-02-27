4a44628e00f131bd4afe9294f3c9ce70a7704291
이민구 <goorm@goormui-MacBookPro.local>
2025-02-27 23:26:33 +0900

    fix: 포트수정

--- src/main/java/org/covy/covycommunitybe/config/CorsConfig.java
+++ src/main/java/org/covy/covycommunitybe/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:3000", "http://example.com"));
+        config.setAllowedOrigins(List.of("http://localhost:5050"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
 

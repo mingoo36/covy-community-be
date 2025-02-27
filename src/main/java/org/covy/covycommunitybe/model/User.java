7b52677cd102a9ebacd4f7fd4d36be89561d18f7
이민구 <goorm@goormui-MacBookPro.local>
2025-02-27 22:50:33 +0900

    feat: 유저 엔티티 생성

---
+++ src/main/java/org/covy/covycommunitybe/model/User.java
@@ -0,0 +1,29 @@
+package org.covy.covycommunitybe.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId; // JPA가 자동으로 user_id로 변환해줌
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String username;
+
+    private String image; // 프로필 이미지 (옵션)
+}
